8871e9f8ac1b9de69c308ad23ce985a85408098e
Jacob Chae <jbcden@gmail.com>
2014-12-27 17:27:20 -0500

    GameState files removed

--- lib/tic_tac_toe/game_state.rb
+++
@@ -1,8 +0,0 @@
-class GameState
-  attr_reader :board, :turn_num, :current_player
-  def initialize(board, turn_num, current_player)
-    @board = board
-    @turn_num = turn_num
-    @player = current_player
-  end
-end
--- test/game_state_test.rb
+++
@@ -1,8 +0,0 @@
-require_relative './test_helper'
-require 'game_state'
-
-class GameStateTest < MiniTest::Test
-  def setup
-    @board = Board.new(3,3)
-  end
-end
