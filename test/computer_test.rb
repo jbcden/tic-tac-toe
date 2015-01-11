47e1743a0da39bfab8c7de65c98a286e25618dcd
Zachary Perlmutter <zrp200@gmail.com>
2015-01-11 12:41:24 -0800

    Update computer_test.rb
    * Replaced hash rockets with hash literals to improve style
    * Removed excess ()s
    Notes:
    * `?` is the same as ""
    * Mostly left actual tests alone; I only really know RSpec

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -4,15 +4,21 @@ require 'board'
 
 class ComputerTest < MiniTest::Test
   def setup
-    @board = Board.new(3,3)
+    @board = Board.new 3, 3
   end
 
   def test_select_winning_move
-    computer = Computer.new("x")
+    computer = Computer.new ?x
     marked_squares = {
-      :a1 => "o", :a2 => " ", :a3 => "x",
-      :b1 => "x", :b2 => " ", :b3 => " ",
-      :c1 => "x", :c2 => "o", :c3 => "o",
+      a1: ?o, 
+      a2: " ",
+      a3: ?x,
+      b1: ?x,
+      b2: " ",
+      b3: " ",
+      c1: ?x,
+      c2: ?o,
+      c3: ?o
     }
 
     @board.board = marked_squares
@@ -23,39 +29,50 @@ class ComputerTest < MiniTest::Test
   end
 
   def test_select_winning_move_depth
-    computer = Computer.new("o")
+    computer = Computer.new ?o
     marked_squares = {
-      :a1 => " ", :a2 => "o", :a3 => " ",
-      :b1 => " ", :b2 => " ", :b3 => "o",
-      :c1 => "x", :c2 => "x", :c3 => "o"
+      a1: " ", 
+      a2: ?o, 
+      a3: " ",
+      b1: " ", 
+      b2: " ", 
+      b3: ?o,
+      c1: ?x, 
+      c2: ?x, 
+      c3: ?o
     }
 
     @board.board = marked_squares
 
-    move = computer.calculate_best_move(@board)
+    move = computer.calculate_best_move @board
 
     assert_equal "a3", move
   end
 
   def test_computer_marks_best_move
-    computer = Computer.new("o")
+    computer = Computer.new ?o 
     marked_squares = {
-      :a1 => " ", :a2 => "o", :a3 => " ",
-      :b1 => " ", :b2 => " ", :b3 => "o",
-      :c1 => "x", :c2 => "x", :c3 => "o"
+      a1: " ",
+      a2: ?o,
+      a3: " ",
+      b1: " ",
+      b2: " ",
+      b3: ?o,
+      c1: ?x,
+      c2: ?x, 
+      c3: ?o
     }
 
     @board.board = marked_squares
 
-    move = computer.calculate_best_move(@board)
-    computer.make_move(@board, move)
+    move = computer.calculate_best_move @board
+    computer.make_move @board, move
 
-    assert_equal true, @board.marked?(move)
-    assert_equal "o", @board.get(move)
+    assert @board.marked? move
+    assert_equal ?o, @board.get move
   end
 
   def test_computer_selects_corner_if_first
-    computer = Computer.new("o")
-    move = computer.calculate_best_move(@board)
+    move = Computer.new(?o).calculate_best_move @board
   end
 end
